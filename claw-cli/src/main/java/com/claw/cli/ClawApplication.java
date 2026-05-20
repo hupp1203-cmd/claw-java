112f0764a0cb1432e25aabdc0b933acd44b0e6d2
hupp <hupp1203@gmail.com>
2026-05-20 09:20:12 +0800

    feat: support -Dclaw.workdir to set workspace directory

--- claw-cli/src/main/java/com/claw/cli/ClawApplication.java
+++ claw-cli/src/main/java/com/claw/cli/ClawApplication.java
@@ -6,7 +6,15 @@ package com.claw.cli;
  */
 public class ClawApplication {
 
+    private static final String WORKDIR_PROP = "claw.workdir";
+
     public static void main(String[] args) {
+        // Allow overriding working directory via system property
+        String workdir = System.getProperty(WORKDIR_PROP);
+        if (workdir != null && !workdir.isBlank()) {
+            System.setProperty("user.dir", workdir);
+        }
+
         ClawContext context = ClawContext.createDefault();
         ClawRepl repl = new ClawRepl(context);
         repl.start();
